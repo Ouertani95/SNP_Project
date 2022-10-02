a73e022a0ba493059f1211d9873a2513ad786fdb
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-10-02 19:17:05 +0200

    added unique constraints

--- SNP_App/models.py
+++ SNP_App/models.py
@@ -3,19 +3,19 @@ from django.db import models
 
 class SNP(models.Model):
     Chrom = models.CharField(max_length=100)
-    Chrom_pos = models.PositiveIntegerField()
-    Rsid = models.CharField(max_length=100)
+    Chrom_pos = models.PositiveIntegerField(unique=True)
+    Rsid = models.CharField(max_length=100, unique=True)
 
 
 class Reference(models.Model):
-    Pubmedid = models.PositiveIntegerField()
+    Pubmedid = models.PositiveIntegerField(unique=True)
     Journal = models.CharField(max_length=100)
-    Title = models.CharField(max_length=1000)
+    Title = models.CharField(max_length=1000, unique=True)
     Date = models.DateField()
 
 
 class DiseaseTrait(models.Model):
-    Disease_name = models.CharField(max_length=100)
+    Disease_name = models.CharField(max_length=100, unique=True)
 
 
 class Association(models.Model):
