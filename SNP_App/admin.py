1d930cc4b0e2855fc7e15a1dcccff3bfc8912a1c
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-10-25 16:02:08 +0200

    registered all tables to admin page

--- SNP_App/admin.py
+++ SNP_App/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import SNP, Association, DiseaseTrait, Reference
 
-# Register your models here.
+
+@admin.register(SNP)
+class SNPAdmin(admin.ModelAdmin):
+    list_display = ('Rsid', 'Chrom', 'Chrom_pos')
+
+
+@admin.register(DiseaseTrait)
+class DiseaseTraitAdmin(admin.ModelAdmin):
+    list_display = ('Disease_name',)
+
+
+@admin.register(Reference)
+class ReferenceAdmin(admin.ModelAdmin):
+    list_display = ('Pubmedid', 'Journal', 'Title', 'Date')
+
+
+@admin.register(Association)
+class AssociationAdmin(admin.ModelAdmin):
+    list_display = ('SNP', 'Disease_trait', 'Reference', 'Pvalue', 'Neglog10pvalue')
