cff2662b130e504e678a13653cec589f528f4742
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-10-25 22:28:05 +0200

    initial working upload with data cleaning

--- SNP_App/scripts/upload_data.py
+++ SNP_App/scripts/upload_data.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from csv import DictReader
 from SNP_App.models import SNP, Reference, DiseaseTrait, Association
+import pandas as pd
 
 
 class FileUploader:
-    REQUIRED_FIELDS = ["DISEASE/TRAIT", "PUBMEDID", "JOURNAL", "STUDY", "DATE", "CHR_ID", "CHR_POS", "SNPS", "P-VALUE",
+    REQUIRED_FIELDS = ["PUBMEDID", "JOURNAL", "STUDY", "DATE", "CHR_ID", "CHR_POS", "SNPS", "DISEASE/TRAIT", "P-VALUE",
                        "PVALUE_MLOG"]
 
     def __init__(self, request_file):
@@ -55,8 +56,20 @@ class FileUploader:
                 snp_id = SNP.objects.get(Chrom_pos=row['CHR_POS'], Rsid=row['SNPS'])
                 disease_trait_id = DiseaseTrait.objects.get(Disease_name=row['DISEASE/TRAIT'])
                 reference_id = Reference.objects.get(Pubmedid=row['PUBMEDID'])
-                association_query = Association.objects.filter(SNP=snp_id.id, Disease_trait=disease_trait_id.id,
-                                                               Reference=reference_id.id)
+                association_query = Association.objects.filter(SNP=snp_id.Rsid
+                                                               , Disease_trait=disease_trait_id.Disease_name
+                                                               , Reference=reference_id.Pubmedid)
                 if not association_query:
                     Association(SNP=snp_id, Disease_trait=disease_trait_id, Reference=reference_id,
                                 Pvalue=row['P-VALUE'], Neglog10pvalue=row['PVALUE_MLOG']).save()
+
+    def clean_entries(self):
+        uploaded_dataframe = pd.read_csv(f'SNP_App/uploads/{self.upload_name}'
+                                         , delimiter="\t"
+                                         , dtype={'CHR_ID': str, 'CHR_POS': str})
+        uploaded_dataframe = uploaded_dataframe.dropna(subset=self.REQUIRED_FIELDS)
+        uploaded_dataframe = uploaded_dataframe[uploaded_dataframe.CHR_ID.apply(lambda x: x.isnumeric())]
+        uploaded_dataframe = uploaded_dataframe[uploaded_dataframe.CHR_POS.apply(lambda x: x.isnumeric())]
+        clean_file_name = "cleaned_"+self.upload_name
+        uploaded_dataframe.to_csv(path_or_buf=f'SNP_App/uploads/{clean_file_name}', sep="\t", index=False)
+        self.upload_name = clean_file_name
