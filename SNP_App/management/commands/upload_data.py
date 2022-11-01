5a974f4059d4c0edcf62e5b74b9c29a9930fce0d
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-11-01 12:15:18 +0100

    added upload_data command to manage.py for file upload using terminal

---
+++ SNP_App/management/commands/upload_data.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from SNP_App.scripts.upload_data_cli import FileUploader
+
+
+class Command(BaseCommand):
+    help = 'Uploads data from file to database'
+
+    def add_arguments(self, parser):
+        parser.add_argument('file_path', nargs=1, type=str)
+
+    def handle(self, *args, **options):
+        file_path = options['file_path'][0]
+        self.stdout.write(file_path)
+        file_uploader = FileUploader(file_path)
+        self.stdout.write("Uploading file ...")
+        file_uploader.upload_file_locally()
+        self.stdout.write("File uploaded successfully !")
+        if file_uploader.has_all_fields():
+            self.stdout.write("Cleaning data ...")
+            file_uploader.clean_entries()
+            self.stdout.write("Data successfully cleaned !")
+            self.stdout.write("Uploading to database ...")
+            file_uploader.upload_content_to_database()
+            self.stdout.write("Data successfully uploaded to database !")
+            self.stdout.write("Removing uploaded files ...")
+            file_uploader.remove_files()
+            self.stdout.write("Files removed successfully !")
+        else:
+            self.stdout.write("Wrong file format !")
+            self.stdout.write("Removing uploaded files ...")
+            file_uploader.remove_files()
+            self.stdout.write("Files removed successfully !")
---
+++ SNP_App/scripts/upload_data_cli.py
@@ -0,0 +1,79 @@
+import os
+import shutil
+from datetime import datetime
+from csv import DictReader
+
+from SNP_App.models import SNP, Reference, DiseaseTrait, Association
+import pandas as pd
+
+
+class FileUploader:
+    REQUIRED_FIELDS = ["PUBMEDID", "JOURNAL", "STUDY", "DATE", "CHR_ID", "CHR_POS", "SNPS", "DISEASE/TRAIT", "P-VALUE",
+                       "PVALUE_MLOG"]
+
+    def __init__(self, request_file):
+        self.file = request_file
+        self.original_name = os.path.basename(self.file)
+        self.upload_name = f"{datetime.now().strftime('%Y_%m_%d_%X')}_{self.original_name}"
+
+    def upload_file_locally(self):
+        shutil.copy2(self.file, f'SNP_App/uploads/{self.upload_name}')
+
+    def has_all_fields(self):
+        with open(f'SNP_App/uploads/{self.upload_name}', 'r') as read_obj:
+            csv_dict_reader = DictReader(read_obj, delimiter="\t")
+            column_names = csv_dict_reader.fieldnames
+            for field in self.REQUIRED_FIELDS:
+                if field not in column_names:
+                    return False
+            return True
+
+    def clean_entries(self):
+        uploaded_dataframe = pd.read_csv(f'SNP_App/uploads/{self.upload_name}',
+                                         delimiter="\t",
+                                         dtype={'CHR_ID': str, 'CHR_POS': str,
+                                                'P-VALUE': float, 'PVALUE_MLOG': float})
+        uploaded_dataframe = uploaded_dataframe.dropna(subset=self.REQUIRED_FIELDS)
+        uploaded_dataframe = uploaded_dataframe[uploaded_dataframe.CHR_POS.apply(lambda x: x.isnumeric())]
+        uploaded_dataframe['CHR_POS'] = uploaded_dataframe['CHR_POS'].astype(int)
+        uploaded_dataframe['PVALUE_MLOG'] = uploaded_dataframe['PVALUE_MLOG'].round(2)
+        clean_file_name = "cleaned_"+self.upload_name
+        uploaded_dataframe.to_csv(path_or_buf=f'SNP_App/uploads/{clean_file_name}', sep="\t", index=False)
+        self.original_name = self.upload_name
+        self.upload_name = clean_file_name
+
+    def upload_content_to_database(self):
+        with open(f'SNP_App/uploads/{self.upload_name}', 'r') as read_obj:
+            csv_dict_reader = DictReader(read_obj, delimiter="\t")
+            for row in csv_dict_reader:
+
+                snp_query = SNP.objects.filter(Rsid=row['SNPS'], Chrom_pos=row['CHR_POS'])
+                if not snp_query:
+                    SNP(Chrom=row['CHR_ID'], Chrom_pos=row['CHR_POS'], Rsid=row['SNPS']).save()
+
+                disease_trait_query = DiseaseTrait.objects.filter(Disease_name=row['DISEASE/TRAIT'])
+                if not disease_trait_query:
+                    DiseaseTrait(Disease_name=row['DISEASE/TRAIT']).save()
+
+                reference_query = Reference.objects.filter(Pubmedid=row['PUBMEDID'])
+                if not reference_query:
+                    Reference(Pubmedid=row['PUBMEDID'], Journal=row['JOURNAL'], Title=row['STUDY'],
+                              Date=row['DATE']).save()
+
+                snp_id = SNP.objects.get(Chrom_pos=row['CHR_POS'], Rsid=row['SNPS'])
+                disease_trait_id = DiseaseTrait.objects.get(Disease_name=row['DISEASE/TRAIT'])
+                reference_id = Reference.objects.get(Pubmedid=row['PUBMEDID'])
+                association_query = Association.objects.filter(SNP=snp_id.Rsid,
+                                                               Disease_trait=disease_trait_id.Disease_name,
+                                                               Reference=reference_id.Pubmedid)
+                if not association_query:
+                    Association(SNP=snp_id, Disease_trait=disease_trait_id, Reference=reference_id,
+                                Pvalue=row['P-VALUE'], Neglog10pvalue=row['PVALUE_MLOG']).save()
+
+    def remove_files(self):
+        original_file_path = f'SNP_App/uploads/{self.original_name}'
+        cleaned_file_path = f'SNP_App/uploads/{self.upload_name}'
+        if os.path.exists(original_file_path):
+            os.remove(original_file_path)
+        if os.path.exists(cleaned_file_path):
+            os.remove(cleaned_file_path)
